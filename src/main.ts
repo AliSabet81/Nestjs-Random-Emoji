ef21cf4086e6439178743639a25d892d92b197d5
AliSabet81 <sabet.farshid1381@gmail.com>
2025-05-19 09:47:32 +0330

    deploy workflow

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalGuards(new AuthGuard());
-  // app.useGlobalInterceptors(new BrowserInterceptor());
-  // app.useGlobalInterceptors(new TransformResponseInterceptor());
-  // app.useGlobalFilters(new AllExceptionsFilter());
   const port = process.env.PORT ?? 3000;
   const host = process.env.HOST ?? '0.0.0.0';
   await app.listen(port, host);
